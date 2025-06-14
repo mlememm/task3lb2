2a677f9aee1b20fe1f59ec585215151e17e22911
mlememm <yun.viviyet@gmail.com>
2025-06-15 01:23:57 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using MyDateTime = System.DateTime;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Демонстрацiя використання псевдонiмiв типiв.");
+        Console.WriteLine("--------------------------------------------");
+        MyDateTime currentTime = MyDateTime.Now;
+        Console.WriteLine($"Поточна дата та час: {currentTime}");
+        Console.WriteLine("--------------------------------------------");
+    }
+}
